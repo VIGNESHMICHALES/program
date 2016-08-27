1de9bdadf90b4895d1e1fb9e14345eb8b994754d
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-08-27 23:12:14 +0530

    Create conver.java

---
+++ conver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class conver
+{ 
+  public static void mzin(Stsring args[])
+  {
+    String str;
+    Scanner sc=new Scanner(System.in);
+    str=sc.nextLine();
+    int num=Integer.parseint();
+    System.out.print(num);
+    }
+  }
