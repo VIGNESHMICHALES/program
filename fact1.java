c9b804e70916a2eff55e0808f8c8e34e3f2b28b4
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-08-27 23:04:57 +0530

    Create fact1.java

---
+++ fact1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fact1
+{
+  public static void main(String args[])
+  {
+  int num;
+  Scanner sc=new Scanner(System.in);
+  num=sc.nextInt();
+  int sum=1;
+  for(int i=1;i<=n;i++)
+  {
+    sum=sum*i;
+  }
+  System.out.println(sum);
+}
+}
+  
