07b47fb952bd2d609d475aae86b12d4b469b1be1
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-08-27 22:56:54 +0530

    Create reversen3.java

---
+++ reversen3.java
@@ -0,0 +1,15 @@
+package coms.basics;
+import java.util.*;
+public class reversen3 {
+	public static void main(String args[])
+	{
+		int num;
+		Scanner sc=new Scanner(System.in); 
+		num=sc.nextInt();
+		String str=String.valueOf(num);
+		StringBuilder sc1=new StringBuilder();
+		sc1.reverse();
+		System.out.print(sc1);
+	}
+}
+	
