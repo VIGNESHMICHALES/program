1a691751d954b05e0f62bc363bb18a08b309869a
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-09-08 22:43:21 +0530

    Update and rename matix.c to matix1.c

---
+++ matix1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	void main() {
+	  char text1[100];
+	  printf("Please enter strings:");
+	  gets(text1);
+	   while (strlen(text1) >= 1){
+	if(strchr(text1,"bbb")!= NULL)
+	  printf("The string is : %s\n",text);
+		 gets(text);
+	}
+}
