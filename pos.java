218bff92a6d3209f9a07544d73b23a5d3a01a584
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-09-08 22:50:43 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class pos
+{
+  public static void main(String ags[])
+  {
+    int num;
+    Scanner sc=new Scanner(System.in);
+    num=sc.nextInt();
+    if(num<0)
+    {
+    System.out.println(num+"is negative');
+  }
+  else
+  {
+  System.out.println(num+"is postive');
+  }
+  }
+  }
