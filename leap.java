e91399dd0b80cb7254080bbbddc0642d8a65d8ce
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-09-10 15:41:04 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class leap
+{
+  public static void main(String ags[])
+  {
+    int num;
+    Scanner sc=new Scanner(System.in);
+    num=sc.nextInt();
+    if(num%4==0)
+    {
+    System.out.println(num+"is leap year');
+  }
+  else
+  {
+  System.out.println(num+"is not leap year');
+  }
+  }
+  }
