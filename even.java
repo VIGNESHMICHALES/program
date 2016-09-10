7d121bc44d4b6628c76a358ee2a08b7c1a0d9a01
VIGNESHMICHALES <vignesh.13bcs2096@mkce.ac.in>
2016-09-10 15:38:39 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class even
+{
+  public static void main(String ags[])
+  {
+    int num;
+    Scanner sc=new Scanner(System.in);
+    num=sc.nextInt();
+    if(num%2==0)
+    {
+    System.out.println(num+"is even');
+  }
+  else
+  {
+  System.out.println(num+"is odd');
+  }
+  }
+  }
